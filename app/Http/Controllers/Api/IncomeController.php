87fe677e0a6637e89fae967abad9ae9bf8bd9d16
Maisho20 <bahajazz70@gmail.com>
2026-02-28 16:26:15 +0700

    feature: Create expense

---
+++ app/Http/Controllers/Api/IncomeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Income;
+use App\Models\UserPocket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class IncomeController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'pocket_id' => 'required|uuid',
+            'amount' => 'required|numeric|min:1',
+            'notes' => 'nullable|string'
+        ]);
+
+        $userId = Auth::guard('api')->id();
+
+        $result = DB::transaction(function () use ($request, $userId) {
+
+            $pocket = UserPocket::where('id', $request->pocket_id)
+                ->where('user_id', $userId)
+                ->firstOrFail();
+            // dd($pocket);
+
+            $income = Income::create([
+                'user_id' => $userId,
+                'pocket_id' => $pocket->id,
+                'amount' => $request->amount,
+                'notes' => $request->notes
+            ]);
+
+            $pocket->increment('balance', $request->amount);
+
+            return [
+                'income' => $income,
+                'pocket' => $pocket->fresh()
+            ];
+        });
+
+        return response()->json([
+            'status' => 200,
+            'error' => false,
+            'message' => 'Berhasil menambahkan income.',
+            'data' => [
+                'id' => $result['income']->id,
+                'pocket_id' => $result['income']->pocket_id,
+                'current_balance' => $result['pocket']->balance
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\IncomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserPocketController;
@@ -10,4 +11,5 @@ Route::middleware('auth:api')->group(function () {
     Route::get('auth/profile', [AuthController::class, 'profile']);
     Route::post('pockets', [UserPocketController::class, 'store']);
     Route::get('pockets', [UserPocketController::class, 'list']);
+    Route::post('incomes', [IncomeController::class, 'store']);
 });
